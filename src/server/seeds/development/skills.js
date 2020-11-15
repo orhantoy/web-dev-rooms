4228e2e2ef0395551934859372d37de7df87886a
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-11-15 21:11:41 +0100

    task#24 db skills table fixed column name for title

--- src/server/seeds/development/skills.js
+++ src/server/seeds/development/skills.js
@@ -5,25 +5,13 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex('skills').insert([
-        { id: 1, colName: 'HTML / CSS' },
-        { id: 2, colName: 'Firebase' },
-        { id: 3, colName: 'Javascript' },
-        {
-          id: 4,
-          title: 'MYSQL',
-        },
-        {
-          id: 5,
-          title: 'NodeJS',
-        },
-        {
-          id: 6,
-          title: 'React',
-        },
-        {
-          id: 7,
-          title: 'Typescript',
-        },
+        { id: 1, title: 'HTML / CSS' },
+        { id: 2, title: 'Firebase' },
+        { id: 3, title: 'Javascript' },
+        { id: 4, title: 'MYSQL' },
+        { id: 5, title: 'NodeJS' },
+        { id: 6, title: 'React' },
+        { id: 7, title: 'Typescript' },
       ]);
     });
 };
