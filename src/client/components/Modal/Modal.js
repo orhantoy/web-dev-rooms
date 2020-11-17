f563389a26d7b1335c286dd4e131045437685f07
prabin <1620prabin@gmail.com>
2020-11-17 12:44:26 +0100

    Storybook: Modal View with Title and closing button X #19

---
+++ src/client/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Modal.css';
+import PropTypes from 'prop-types';
+
+export default function Modal({ title }) {
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-header">
+        <span className="close-modal-btn">x</span>
+      </div>
+      <h4>{title} </h4>
+    </div>
+  );
+}
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+};
