9d8176f04eb0e0b4876fe060a527659a45c15167
claudion3 <claudion3@gmail.com>
2020-11-18 22:28:57 +0100

    fixed addon-actions version and onClick function

--- src/client/components/SignInButton/Button.js
+++ src/client/components/SignInButton/Button.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Button.css';
-import { action } from '@storybook/addon-actions';
 
-const Button = () => {
+const Button = ({ children, click }) => {
+  const clicked = () => {
+    click();
+  };
   return (
-    <button type="submit" className="button" onClick={action('clicked')}>
-      <img
-        alt="Sign in with Slack"
-        src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
-      />
+    <button type="submit" className="button" onClick={clicked}>
+      {children}
     </button>
   );
 };
--- src/client/components/SignInButton/Button.stories.js
+++ src/client/components/SignInButton/Button.stories.js
@@ -8,4 +8,12 @@ export default {
   component: Button,
 };
 
-export const SignButton = () => <Button onClick={action('clicked')} />;
+export const SignButton = () => (
+  <Button click={action('clicked')}>
+    <img
+      alt="Sign in with Slack"
+      src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+      srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+    />
+  </Button>
+);
