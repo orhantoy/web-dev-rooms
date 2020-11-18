7b36798dfe40492d6a33b37f464c5ebc7d86355a
Pallabi Ghosh Das <pallabighosh3100@gmail.com>
2020-11-18 17:45:08 +0100

    To revert unwanted changes

--- src/client/components/Heading/Heading.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Heading() {
-  return <Heading variant="heading">Study groups</Heading>;
-}
--- src/client/components/Heading/heading.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Heading from './Heading';
-
-export default { title: 'studyGroups' };
-export const studyGroupsHeading = () => <Heading />;
