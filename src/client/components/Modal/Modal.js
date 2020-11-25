4e5496b27bf8691eefc427b6cbc116b88e424c16
prabin <1620prabin@gmail.com>
2020-11-25 12:29:35 +0100

    passing children property in this component

--- src/client/components/Modal/Modal.js
+++ src/client/components/Modal/Modal.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './Modal.css';
 import PropTypes from 'prop-types';
 
-export default function Modal({ title }) {
+export default function Modal(props) {
   return (
     <div className="modal-wrapper">
       <div className="modal-header">
         <span className="close-modal-btn">x</span>
       </div>
-      <h4 className="modal-title">{title} </h4>
+      <h4 className="modal-title">{props.title} </h4>
+      {props.children}
     </div>
   );
 }
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
