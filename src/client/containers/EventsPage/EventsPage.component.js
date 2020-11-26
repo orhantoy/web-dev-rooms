8d1078c7af47d2f8e05359f442e34c800940ccc7
Deepti <deepti.sharma@gmail.com>
2020-11-26 19:55:16 +0100

    Dummy pages created

---
+++ src/client/containers/EventsPage/EventsPage.component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EventsPage() {
+  return <h1>This is a EventsPage</h1>;
+}
---
+++ src/client/containers/RegistrationPage/RegistrationPage.component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RegistrationPage() {
+  return <h1>This is a RegistrationPage</h1>;
+}
