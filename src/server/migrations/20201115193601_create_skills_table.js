e2628405100cc77ed5f4a1723d0bd1f6f9df2310
Deepti <deepti.sharma@gmail.com>
2020-11-26 17:12:42 +0100

    Merged 'skills' table from week1

--- src/server/migrations/20201115193601_create_skills_table.js
+++ src/server/migrations/20201115193601_create_skills_table.js
@@ -1,10 +1,12 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('skills', (table) => {
     table.increments();
-    table.string('title').notNullable();
+    table.string('name').notNullable();
+    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('deleted_at');
   });
 };
-
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('skills');
 };
