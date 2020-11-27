093e18a1d892ff31d6a53170e6732bd167275cb6
claudion3 <claudion3@gmail.com>
2020-11-27 03:22:26 +0100

    implement selection page

---
+++ src/client/components/SelectionPage/Selectionpage.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SelectOption from './SelectionPage';
+
+// import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Seletion page',
+  component: SelectOption,
+};
+
+export const SelectionPage = () => <SelectOption />;
