61f2cd1d65451e57394284bfb55eab3673ebf7fb
Elia-Git <holeselv17@gmail.com>
2020-11-29 15:08:46 +0100

    Final modification and commented modification will be applied later

---
+++ src/server/migrations/20201129222522_create_questions_table.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('questions', (table) => {
+    table.increments();
+    table.integer('topic').unsigned();
+    table
+      .integer('asked_by')
+      .unsigned()
+      .references('id')
+      .inTable('users');
+    table.integer('event_id').unsigned();
+    table
+      .datetime('created_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table
+      .datetime('updated_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.datetime('deleted_at');
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('questions');
+};
