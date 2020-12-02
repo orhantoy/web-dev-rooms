9c61034a16d19d04242123e167e394f4560648cd
claudion3 <claudion3@gmail.com>
2020-12-02 21:59:38 +0100

    modifying max_Participants to max_participants

--- src/server/migrations/20201202203354_modifying_maxParticipants_in_events.js
+++ src/server/migrations/20201202203354_modifying_maxParticipants_in_events.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.table('events', function (t) {
-    t.renameColumn('max_Participants', 'maxParticipants');
+    t.renameColumn('max_participants', 'maxParticipants');
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table('events', function (t) {
-    t.renameColumn('max_Participants', 'maxParticipants');
+    t.renameColumn('max_participants', 'maxParticipants');
   });
 };
