e89f22f96880b8fad5a534b970295b12bbe51995
claudion3 <claudion3@gmail.com>
2020-12-02 21:33:04 +0100

    fix errors

--- src/server/migrations/20201202203354_modifying_maxParticipants_in_events.js
+++ src/server/migrations/20201202203354_modifying_maxParticipants_in_events.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.table('events', function (t) {
-    t.renameColumn('maxParticipants', 'max_Participants');
+    t.renameColumn('max_Participants', 'maxParticipants');
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table('events', function (t) {
-    t.renameColumn('maxParticipants', 'max_Participants');
+    t.renameColumn('max_Participants', 'maxParticipants');
   });
 };
