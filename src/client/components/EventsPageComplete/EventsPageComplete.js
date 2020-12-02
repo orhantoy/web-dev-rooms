de81752ab4a568c5ecddc651ebc213f78bb1aaf1
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-12-02 21:36:18 +0100

    eventpagecomplete

--- src/client/components/EventsPageComplete/EventsPageComplete.js
+++ src/client/components/EventsPageComplete/EventsPageComplete.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import EventsPage from '../EventsPage/EventsPage.component';
+import PropTypes from 'prop-types';
+// import EventsPage from '../EventsPage/EventsPage.component';
 import { AppHeader } from '../Appheader/AppHeader.component';
 import Footer from '../footer/footer';
 import './EventsPageComplete.styles.css';
 import Header from '../Heading/Heading';
+import { CardWithEventsOrStudyGroups } from '../CardWithEventsOrStudyGroups/CardWithEventsOrStudyGroups';
 
 function EventsPageComplete() {
   return (
     <div className="events-page">
-      <AppHeader className="app-header" />
-      <Header />
-      <EventsPage />
-      <Footer />
+      <AppHeader id="app-header" />
+      <Header id="header" />
+      <CardWithEventsOrStudyGroups id="event-card" />
+      <Footer id="footer" />
     </div>
   );
 }
+
+CardWithEventsOrStudyGroups.propTypes = {
+  path: PropTypes.string,
+  children: PropTypes.node,
+  title: PropTypes.string,
+  datetime: PropTypes.string,
+  mentor: PropTypes.string,
+  link: PropTypes.string,
+};
+
+CardWithEventsOrStudyGroups.defaultProps = {
+  path: '',
+  children: '',
+  title: '',
+  datetime: '',
+  mentor: '',
+  link: '',
+};
 
 export default EventsPageComplete;
--- src/client/components/EventsPageComplete/EventsPageComplete.stories.js
+++ src/client/components/EventsPageComplete/EventsPageComplete.stories.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import EventsPageComplete from './EventsPageComplete';
+import ElasticLogo from '../StudyGroupLogo/Elastic.logo.svg';
 
 export default { title: 'EventsPageComplete' };
 
-export const Complete = () => <EventsPageComplete />;
+export const Complete = () => (
+  <EventsPageComplete
+    path={ElasticLogo}
+    title="Elastic - Week 46"
+    datetime="Thursday | 12 Nov 20|13:00 CEST"
+    mentor="Orhan"
+    link="www.hyf.org"
+  >
+    Register Yourself
+  </EventsPageComplete>
+);
