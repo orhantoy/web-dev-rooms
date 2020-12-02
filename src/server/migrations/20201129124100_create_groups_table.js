612cecac6e7dbed532fd6d9613b936331bca09ae
Elia-Git <holeselv17@gmail.com>
2020-12-02 18:34:54 +0100

    Feedback commited that corresponds to latest tables diagram

--- src/server/migrations/20201129124100_create_groups_table.js
+++ src/server/migrations/20201129124100_create_groups_table.js
@@ -2,13 +2,6 @@ exports.up = function (knex) {
   return knex.schema.createTable('groups', (table) => {
     table.increments();
     table.string('name').notNullable();
-    table
-      .integer('user_id')
-      .unsigned()
-      .notNull()
-      .references('id')
-      .inTable('users')
-      .onDelete('cascade');
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
