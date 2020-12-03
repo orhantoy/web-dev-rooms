b3d8b1dc4ecc699cb54250c154b677300488e888
nuha90 <nuhanajah90@gmail.com>
2020-12-03 09:46:03 +0100

    change components styling

--- src/client/components/SignInButton/Button.js
+++ src/client/components/SignInButton/Button.js
@@ -8,13 +8,15 @@ const Button = (props) => {
     onclick();
   };
   return (
-    <button type="submit" className="button" onClick={clicked}>
-      <img
-        alt="Sign in with Slack"
-        src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
-      />
-    </button>
+    <div className="box">
+      <button type="submit" className="button" onClick={clicked}>
+        <img
+          alt="Sign in with Slack"
+          src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+          srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+        />
+      </button>
+    </div>
   );
 };
 // add propType for onclick function
